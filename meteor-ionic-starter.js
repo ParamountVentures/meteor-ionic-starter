e0dddb44e49aefa7fd5f782cc4071a8a99059674
Steven Livingstone-Perez <steven@livz.org>
2015-09-22 15:44:20 +0100

    Calling a controller in angular from the ionic front end via meteor for web and mobile.

--- meteor-ionic-starter.js
+++ meteor-ionic-starter.js
@@ -1,21 +1,23 @@
 if (Meteor.isClient) {
  var app = angular.module('starter', [
      'angular-meteor',
+     'ui.router',
      'ionic'
    ]
  );
 
   function onReady() {
     angular.bootstrap(document, ['starter']);
-    console.log('Ready');
   }
 
   // the web and cordova fire different events for readiness
   if (Meteor.isCordova) {
-    console.log('Preparing Cordova');
     angular.element(document).on('deviceready', onReady);
   } else {
-    console.log('Preparing Web');
     angular.element(document).ready(onReady);
   }
+
+  app.controller('TestController', function() {
+    this.message = 'Hello from the Angular Controller';
+  });
 }
